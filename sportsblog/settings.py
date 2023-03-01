180640b1c523c696066905ab82480eae11793885
seanj06 <seanj06@hotmail.com>
2023-03-01 16:06:06 +0000

    Add allauth to installed apps and variables

--- sportsblog/settings.py
+++ sportsblog/settings.py
@@ -40,11 +40,19 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary',
     'home',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
