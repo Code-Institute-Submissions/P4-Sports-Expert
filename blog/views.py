09fa19be637ac0f9f1aea8a2d9839eadc6296bdc
seanj06 <seanj06@hotmail.com>
2023-03-07 11:20:04 +0000

    Add success messages to addblog and editblog classes

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.views.generic import DeleteView
 from django.http import HttpResponseForbidden
@@ -20,39 +20,26 @@ class BlogDetail(DetailView):
     template_name = 'blog_detail.html'
 
 
-class AddBlog(LoginRequiredMixin, CreateView):
+class AddBlog(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = BlogPost
     template_name = 'add_blog.html'
     form_class = BlogForm
+    success_url = reverse_lazy('bloglist')
+    success_message = "Blog post created successfully"
 
     def get_initial(self):
         return {'created_by': self.request.user}
 
 
-class EditBlog(UpdateView):
+class EditBlog(SuccessMessageMixin, UpdateView):
     model = BlogPost
     template_name = 'edit_blog.html'
     fields = ['description', 'title', 'blog_image', 'body', 'category']
-    # Checks if logged in user is owner of the post and has permission to edit
+    success_url = reverse_lazy('bloglist')
+    success_message = "Blog post edited successfully"
 
-    def dispatch(self, request, *args, **kwargs):
-        handler = super(EditBlog, self).dispatch(request, *args, **kwargs)
-        if self.object.created_by != request.user:
-            return HttpResponseForbidden("You are not the owner of this blog")
-            return handler
-        else:
-            return handler    
-        
 
-class DeleteBlog(DeleteView):
+class DeleteBlog(SuccessMessageMixin, DeleteView):
     model = BlogPost
     template_name = 'delete_blog.html'
-    success_url = reverse_lazy('home')
-
-    def dispatch(self, request, *args, **kwargs):
-        handler = super(DeleteBlog, self).dispatch(request, *args, **kwargs)
-        if self.object.created_by != request.user:
-            return HttpResponseForbidden("You are not the owner of this blog")
-            return handler
-        else:
-            return handler    
+    success_url = reverse_lazy('bloglist')
