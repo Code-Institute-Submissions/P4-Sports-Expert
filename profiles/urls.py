d607845a3a4d0801dd07dc26c01e7b254bbb6f32
seanj06 <seanj06@hotmail.com>
2023-03-09 12:43:48 +0000

    Add Addprofile view and url path

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProfileView
+from .views import ProfileView, AddProfile
 
 
 urlpatterns = [
     path('', ProfileView.as_view(), name="profileview"),
+    path('add_profile/', AddProfile.as_view(), name='add_profile')
+
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from .models import Profile
 from blog.models import BlogPost
+from .forms import ProfileForm
 
 
 class ProfileView(ListView):
     model = Profile
     template_name = 'profile.html'
+
+
+class AddProfile(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Profile
+    template_name = 'add_profile.html'
+    form_class = ProfileForm
+    success_url = reverse_lazy('profileview')
+    success_message = "Profile created successfully"
+
+    def get_initial(self):
+        return {'created_by': self.request.user}
