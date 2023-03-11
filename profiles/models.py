b699748221ea0b0477337b538c0bbbb39614c2b8
seanj06 <seanj06@hotmail.com>
2023-03-11 15:41:55 +0000

    Add date joined field to model and profile template

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from datetime import datetime, date
 from cloudinary.models import CloudinaryField
 from blog.models import BlogPost
 from django.dispatch import receiver
@@ -19,6 +20,7 @@ class Profile(models.Model):
     blogs = models.ManyToManyField(
         BlogPost, related_name="blogs", blank=True,
     )
+    date_joined = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.user.username
