d6ee8732dea098475e2c6db6a810ed58bdc81a07
seanj06 <seanj06@hotmail.com>
2023-03-25 18:24:17 +0000

    Add unit tests for blogform

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,1 +1,47 @@
 from django.test import TestCase
+from .forms import BlogForm, CommentForm
+
+
+class TestBlogForm(TestCase):
+
+    def test_blogform_is_valid(self):
+        """Test blog form is valid"""
+        form = BlogForm(
+            {
+                "created_by": "superuser90",
+                "description": "This is a football post",
+                "title": "Football Post",
+                "blog_image": "test.jpg",
+                "body": "This is the post body",
+                "category": "Football",
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_blogform_body_is_required(self):
+        form = BlogForm(
+            {
+                "created_by": "superuser90",
+                "description": "This is a football post",
+                "title": "Football Post",
+                "blog_image": "test.jpg",
+                "body": "",
+                "category": "Football",
+            }
+            )
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
+    def test_image_field_is_not_required(self):
+        form = BlogForm(
+            {
+                "created_by": "superuser90",
+                "description": "This is a football post",
+                "title": "Football Post",
+                "blog_image": "",
+                "body": "Test body",
+                "category": "Football",
+            }
+        )
+        self.assertTrue(form.is_valid())
