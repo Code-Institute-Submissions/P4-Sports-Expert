3d2bbe405611e78eb098c73c05d1c57878ad5ec3
seanj06 <seanj06@hotmail.com>
2023-03-26 13:59:42 +0000

    Add unit tests for comment model

--- blog/test_models.py
+++ blog/test_models.py
@@ -54,3 +54,45 @@ class TestBlogPostModel(TestCase):
             f"Blog post by {test_post.created_by} on {test_post.date_created}"
         )
         self.assertEqual(str(test_post), test_string)
+
+
+class TestCommentModel(TestCase):
+    """Unit Tests for comments model"""
+    def setUp(self):
+        """Creates valid comment database entry"""
+        # Needed for created_by id non null constraint
+        test_user = User.objects.create_user(
+            username='testuser2', password='testpassword1'
+        )
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+        # Same blogpost entry as Test Blogpost model used here
+        self.blog_post = BlogPost.objects.create(
+            created_by=test_user,
+            description="This is a test",
+            title="This is a test blogpost title",
+            body='This is a test blogpost body',
+            category='Football',
+        )
+
+    def test_comment_is_valid(self):
+        """Tests that asserted values are valid"""
+        comment = Comments.objects.create(
+            post=self.blog_post,
+            comment='This is a test comment.',
+            user=self.user,
+        )
+        self.assertEqual(comment.post, self.blog_post)
+        self.assertEqual(comment.comment, 'This is a test comment.')
+        self.assertEqual(comment.user, self.user)
+
+    def test_comment__str__(self):
+        """ Tests __str__ method returns correct value"""
+        comment = Comments.objects.create(
+            post=self.blog_post,
+            comment='This is a test comment.',
+            user=self.user,
+        )
+        self.assertEqual(
+            str(comment), f"Comment by {self.user} on {comment.time}"
+            )
