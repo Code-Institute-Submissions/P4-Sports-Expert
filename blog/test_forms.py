5caa5b97e6c95d44a0ed0c7bdc40738aae22b533
seanj06 <seanj06@hotmail.com>
2023-03-26 11:25:34 +0000

    Add unit tests for comment form

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from .forms import BlogForm, CommentForm
+from .models import BlogPost
 
 
 class TestBlogForm(TestCase):
-
+    """Unit tests for blogform"""
     def test_blogform_is_valid(self):
         """Test blog form is valid"""
         form = BlogForm(
@@ -33,15 +34,31 @@ class TestBlogForm(TestCase):
         self.assertIn('body', form.errors.keys())
         self.assertEqual(form.errors['body'][0], 'This field is required.')
 
-    def test_image_field_is_not_required(self):
+    def test_image_field_must_be_file(self):
         form = BlogForm(
             {
                 "created_by": "superuser90",
                 "description": "This is a football post",
                 "title": "Football Post",
-                "blog_image": "",
+                "blog_image": 0,
                 "body": "Test body",
                 "category": "Football",
             }
         )
+        self.assertFalse(form.is_valid())
+
+
+class TestCommentForm(TestCase):
+    """Unit tests for comment form"""
+    def comment_form_is_valid(self):
+        """Test form is valid"""
+        form = CommentForm({"comment": "Great write up"})
         self.assertTrue(form.is_valid())
+
+    def test_commentform_body_empty(self):
+        """Test comment form is invalid with empty body"""
+        form = CommentForm({"comment": ""})
+        self.assertIn("comment", form.errors.keys())
+        self.assertEqual(form.errors["comment"][0], "This field is required.")
+        self.assertFalse(form.is_valid())
+
